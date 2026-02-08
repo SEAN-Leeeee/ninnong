ce647035449d37d7700e5f338afd28e1ffe38fe1
sean <siyeon0805@kakao.com>
2026-02-08 23:35:13 +0900

    feat: cors 설정

--- src/main/java/com/sean/ninnong/config/WebConfig.java
+++ src/main/java/com/sean/ninnong/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "https://ninnong.cloud", "https://www.ninnong.cloud")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowCredentials(true);
     }
